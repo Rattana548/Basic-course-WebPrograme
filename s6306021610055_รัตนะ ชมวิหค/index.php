8722995c5c044d7286cb54f6e84474888e7ebbc3
Rattana548 <68465498+Rattana548@users.noreply.github.com>
2022-02-26 20:39:34 +0700

    Add files via upload

---
+++ s6306021610055_รัตนะ ชมวิหค/index.php
@@ -0,0 +1,91 @@
+<html>
+	<head>
+ 		<title>s6306021610055 รัตนะ  ชมวิหค     </title>
+		<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">
+		<link rel="stylesheet" href="css.css">
+	</head>
+
+	<body>
+	<?php
+		echo "<div class=\"con\">
+		
+			<h2><br>แนะนำตัวเอง</h2><img src=\"233456600_822316828426779_5989950702805414835_n.jpg\">
+			<h4>Name : Rattana Chomwihok<br><br>ชื่อ : รัตนะ ชมวิหค<br><br>
+			รหัสนักศึกษา : 6306021610055<br><br>สาขา : เทคโนโลยีสารสนเทศ<br><br>
+			คณะ : เทคโนโลยีและการจัดการอุตสาหกรรม<br><br>มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ เขตปราจีนบุรี<br><br><br><br>
+			Fackbook : <a href=\"https://www.facebook.com/jkloiiu/\" target=\"_top\">Rattana Chomwihok</a>
+			<br><br>
+			</h4>
+		
+		</div>
+		<table>
+		<thead>
+			<tr class=\"height100\">
+				<th>ลำดับ</th>
+				<th class=\"width1\">ละดับชั้น</th>
+				<th class=\"width2\">โรงเรียน</th>
+				<th class=\"width1\">จังหวัด</th>
+			</tr>
+		</thead>
+		  <tr>
+			<td>1</td>
+			<td>ประถมศึกษา</td>
+			<td class=\"aling\">โรงเรียนชุมชนวันราษฏบำรุง</td>
+			<td>สมุทรปราการ</td>
+		  </tr>
+		  <tr>
+			<td>2</td>
+			<td>มัธยม 1-2</td>
+			<td class=\"aling\">นวมินทราชินูทิศสวนกุหลาบวิทยาลัยสมุทรปราการ</td>
+			<td>สมุทรปราการ</td>
+		  </tr>
+		  <tr>
+			<td>3</td>
+			<td>มัธยม 3</td>
+			<td class=\"aling\">การศึกษานอกระบบและการศึกษาตามอัธยาศัย จังหวัดสมุทรปราการ</td>
+			<td>สมุทรปราการ</td>
+		  </tr>
+		  <tr>
+			<td>4</td>
+			<td>ปวช 1-3</td>
+			<td class=\"aling\">วิทยาลัยเทคโนโลยีภาคตะวันออกอีเทค</td>
+			<td>ชลบุรี</td>
+		  </tr>
+		  <tr>
+			<td>5</td>
+			<td>ปริญญาตรี</td>
+			<td class=\"aling\">มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ เขตปราจีน</td>
+			<td>ปราจีนบุรี</td>
+		  </tr>
+		</table>
+		<br><br><br><br>
+		<table>
+		<thead>
+			<tr class=\"height100\">
+				<th>ลำดับ</th>
+				<th class=\"width1\">งานอดิเรก</th>
+			</tr>
+		</thead>
+		  <tr>
+			<td>1</td>
+			<td>เล่นเกม</td>
+		  </tr>
+		  <tr>
+			<td>2</td>
+			<td>ดูหนัง</td>
+		  </tr>
+		  <tr>
+			<td>3</td>
+			<td>ดูการ์ตูน</td>
+		  </tr>
+		  <tr>
+			<td>4</td>
+			<td>อ่านมังงะ</td>
+		  </tr>
+		</table>
+		<br><br>
+		";
+		?>
+	</body>
+
+</html> 
